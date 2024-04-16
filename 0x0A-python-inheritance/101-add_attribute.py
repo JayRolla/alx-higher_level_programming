61349e8c6fa83728e5005a7fff24deefe333aff3
JayRolla <molapokgarose@gmail.com>
2024-04-16 14:29:29 -0700

    retry

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """BaseGeometry class with integer validation."""
-    
-    def area(self):
-        """Placeholder for area computation."""
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """Validate that the 'value' is a positive integer."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+"""
+This module defines a function add_attribute that adds a new attribute
+to an object if it's possible.
+"""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """
+    Add a new attribute to an object if it's possible.
+    Raises a TypeError if the attribute cannot be added.
+    """
+    if '__dict__' in dir(obj) or '__slots__' in dir(obj):
+        if hasattr(obj, '__slots__') and not hasattr(obj, attr_name):
+            raise TypeError("can't add new attribute")
+        setattr(obj, attr_name, attr_value)
+    else:
+        raise TypeError("can't add new attribute")
